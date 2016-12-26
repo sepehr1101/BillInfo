17624922a6e7dff501ce8da3d2396047bf88937c
_1101 <mantera.sh@gmail.com>
2016-12-26 08:38:59 +0330

    Add project files.

---
+++ BillInfoNew/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillInfoNew
+{
+    public class Class1
+    {
+    }
+}
